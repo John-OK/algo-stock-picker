053f8faf26fd4c4d4b027305f8f1eaad01771adc
JohnOK <john.ok.esp@gmail.com>
2022-06-06 20:58:35 +0200

    Added python version of stock picker and spec file tests.

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,21 @@
+# 1. Iterate through each number in array and iterate through other nums to find largest difference for that number.
+# 2. Check if diff between nums (higher index num - lower index num) is greater than current max, if so, store indices of the pair: [lower index, higher index]
+# 3. Return array of pair of indices
+
+
 def picker(prices):
-    pass
+    max_profit = 0
+    max_profit_pair = []
+
+    for i in range(len(prices) - 1):
+        buy = prices[i]
+        for j in range(i + 1, len(prices)):
+            sell = prices[j]
+            profit = sell - buy
+            if profit > max_profit:
+                max_profit = profit
+                max_profit_pair = [i, j]
+    return max_profit_pair
+
+# days = picker([1,2])
+# print(days)
